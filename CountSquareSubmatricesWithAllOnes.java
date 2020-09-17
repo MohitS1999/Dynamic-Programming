72ef8ab9b92902a48e5aa00df1dbda98ee11c904
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-09-17 21:19:46 +0530

    Medium

--- CountSquareSubmatricesWithAllOnes.java
+++ CountSquareSubmatricesWithAllOnes.java
@@ -43,23 +43,3 @@ public class CountSquareSubmatricesWithAllOnes{
 		}
 	}	
 }
-// class Solution {
-    // public int countSquares(int[][] matrix) {
-        // int row=matrix.length;
-        // if (row==0) return 0;
-        // int col=matrix[0].length;
-        // int dp[][]=new int[row+1][col+1];
-        // int totalCount=0;
-        // for (int i=1;i<=row;i++){
-            // for (int j=1;j<=col;j++){
-                // if (matrix[i-1][j-1]==0)
-                    // dp[i][j]=0;
-                // else {
-                    // dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
-                    // totalCount+=dp[i][j];
-                // }
-            // }
-        // }
-        // return totalCount;
-    // }
-// }
